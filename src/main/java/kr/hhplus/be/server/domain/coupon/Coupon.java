8d3238d8fdb3d12a827e1c0f5d38c8fc2a9991a9
최연영 <>
2025-04-12 16:15:13 +0900

    fix: 파일구조 변경 누락 내용 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/coupon/Coupon.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.domain.coupon;
+
+public class Coupon {
+}
---
+++ src/main/java/kr/hhplus/be/server/infra/point/repository/PointRepositoryImpl.java
@@ -0,0 +1,31 @@
+package kr.hhplus.be.server.infra.point.repository;
+
+import kr.hhplus.be.server.domain.point.Point;
+import kr.hhplus.be.server.domain.point.PointRepository;
+import kr.hhplus.be.server.infra.point.jpa.PointJpaRepository;
+import kr.hhplus.be.server.infra.point.jpa.PointEntity;
+import kr.hhplus.be.server.infra.point.mapper.PointMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@RequiredArgsConstructor
+public class PointRepositoryImpl implements PointRepository {
+
+    private final PointJpaRepository pointJpaRepository;
+
+    @Override
+    public Point findByUserId(Long userId){
+        return pointJpaRepository.findByUserId(userId)
+                .map(PointMapper::toDomain)
+                .orElseThrow(() -> new IllegalArgumentException("포인트 없음"));
+    }
+
+    @Override
+    public void save(Point point){
+        PointEntity entity = PointEntity.toEntity(point);
+        pointJpaRepository.save(entity);
+    }
+}
