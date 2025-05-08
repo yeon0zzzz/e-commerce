65cbf8e1674e619a37fa529118b90ccab8dd5793
최연영 <>
2025-05-09 06:58:37 +0900

    Test: 캐시 저장 테스트

---
+++ src/test/java/kr/hhplus/be/server/cache/PopularProductCacheTest.java
@@ -0,0 +1,55 @@
+package kr.hhplus.be.server.cache;
+
+import jakarta.transaction.Transactional;
+import kr.hhplus.be.server.interfaces.RedisRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@DisplayName("인기 상품 호출 캐시 테스트")
+public class PopularProductCacheTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    RedisRepository redisRepository;
+
+    private final String redisCacheKey = "popularProducts::popularProducts";  // 기본 key 규칙
+
+    @BeforeEach
+    void setUp() {
+        redisRepository.remove(redisCacheKey);
+    }
+
+    @Test
+    @DisplayName("인기 상품 API 호출 후 Redis에 캐시가 저장된다.")
+    @Sql(scripts = "/sql/popularProducts.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void getPopularProducts_shouldBeCachedInRedis() throws Exception {
+
+        // 1. 캐시 사전 존재 여부 확인
+        assertThat(redisRepository.keyExists(redisCacheKey)).isFalse();
+
+        // 2. 컨트롤러 호출
+        mockMvc.perform(get("/stats/products/popular"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.popularProducts.length()").value(5));
+
+        // 3. 캐시 존재 여부 확인
+        assertThat(redisRepository.keyExists(redisCacheKey)).isTrue();
+
+    }
+}
