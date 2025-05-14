1b6bff7c8fa23aaaf97571817f99b4443b99fc81
최연영 <>
2025-05-15 02:32:22 +0900

    Feat: Redis Sorted Set 기반의 랭킹 제공 로직 구현

---
+++ src/main/java/kr/hhplus/be/server/application/order/OrderCompletedEventListener.java
@@ -0,0 +1,19 @@
+package kr.hhplus.be.server.application.order;
+
+import kr.hhplus.be.server.domain.product.stats.ProductSalesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@RequiredArgsConstructor
+@Component
+public class OrderCompletedEventListener {
+
+    private final ProductSalesService productSalesService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(OrderCompletedEvent event) {
+        productSalesService.increaseDailySales(event.items());
+    }
+}
---
+++ src/main/java/kr/hhplus/be/server/infra/product/stats/ProductSalesRepositoryImpl.java
@@ -0,0 +1,64 @@
+package kr.hhplus.be.server.infra.product.stats;
+
+import kr.hhplus.be.server.domain.product.stats.ProductSalesDaily;
+import kr.hhplus.be.server.domain.product.stats.ProductSalesRepository;
+import kr.hhplus.be.server.infra.RedisRepository;
+import kr.hhplus.be.server.infra.product.stats.jpa.ProductSalesDailyEntity;
+import kr.hhplus.be.server.infra.product.stats.jpa.ProductSalesDailyJpaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Set;
+
+@Repository
+@AllArgsConstructor
+public class ProductSalesRepositoryImpl implements ProductSalesRepository {
+
+    private final RedisRepository redisRepository;
+    private final ProductSalesDailyJpaRepository productSalesDailyJpaRepository;
+
+    public void increaseDailySales(String key, String value, Long score) {
+        redisRepository.incrementSortedSet(key, value, score);
+        redisRepository.expire(key, Duration.ofDays(2));
+    }
+
+    public List<ProductSalesDaily> findAll(String key) {
+        Set<ZSetOperations.TypedTuple<Object>> salesData = redisRepository.getSortedSetRangeWithScore(key, 0L, -1L);
+
+        if (salesData == null) return List.of();
+
+        LocalDateTime salesAt = LocalDate.parse(key.substring("product:sales:".length()),
+                        DateTimeFormatter.ofPattern("yyyyMMdd"))
+                .atStartOfDay();
+
+        return salesData.stream()
+                .filter(entry -> entry.getValue() != null && entry.getScore() != null)
+                .map(entry -> ProductSalesDaily.create(
+                        salesAt,
+                        Long.valueOf(entry.getValue().toString()),
+                        entry.getScore().longValue()
+                ))
+                .toList();
+    }
+
+    public void saveAll(List<ProductSalesDaily> products) {
+        List<ProductSalesDailyEntity> entities2 = products.stream()
+                .map(ProductSalesDailyEntity::toEntity)
+                .toList();
+
+        productSalesDailyJpaRepository.saveAll(entities2);
+    }
+
+    public ProductSalesDaily findById(Long id) {
+       return productSalesDailyJpaRepository.findById(id)
+               .map(ProductSalesDailyEntity::toDomain)
+               .orElseThrow(() -> new IllegalArgumentException("일별 집계 상품이 없습니다."));
+    }
+
+}
